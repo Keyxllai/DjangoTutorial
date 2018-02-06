0bd3fb8a86e6cc485c7bfa40e37bb9abd84e1208
sl8b <shaun.x.lai@newegg.com>
2018-02-06 19:40:19 +0800

    part 3

--- votesite/polls/views.py
+++ votesite/polls/views.py
@@ -1,4 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
+from django.template import loader
+from django.shortcuts import render
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello Polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    #response = "Looking at question %s."
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNoteExist:
+        raise Http404("Question does not exist.")
+    return render(request,'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    return HttpResponse("Looking at resuls of question %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Looking at vote on question %s" % question_id)
