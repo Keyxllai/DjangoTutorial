c334c773bdc8e55d5d641652a045daa4ff75b934
keyxllai <keyxllai@gmail.com>
2018-02-07 23:06:29 +0800

    update

--- votesite/polls/views.py
+++ votesite/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse,Http404
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 from .models import Question
 
@@ -14,10 +14,11 @@ def index(request):
 
 def detail(request, question_id):
     #response = "Looking at question %s."
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNoteExist:
-        raise Http404("Question does not exist.")
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNoteExist:
+    #     raise Http404("Question does not exist.")
+    question =  get_object_or_404(Question,pk=question_id)
     return render(request,'polls/detail.html', {'question': question})
 
 def results(request, question_id):
